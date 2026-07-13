80919c030623eed54ee3bf57f240ee33522cba8c
Techsavy-Maahir <maahirrayaan365@gmail.com>
2026-07-13 20:47:13 +0530

    Add user registration with bcrypt password hashing

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const User = require('./models/User');
 
 const app = express();
 
@@ -21,6 +23,34 @@ app.get('/api/test', (req, res) => {
     message: 'Server is running!'
   });
 });
+
+app.post('/api/auth/register',async (req,res) =>{
+
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if(existingUser) {
+    return res.status(400).json({
+      message: 'User already exists'
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({
+    email,
+    password: hashedPassword
+  });
+
+  await user.save();
+
+  res.json({
+    message : "Registered",
+    userId : user._id
+  });
+});
+
+console.log("NEW SERVER CODE LOADED");
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
